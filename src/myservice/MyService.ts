f6c87c8217b824e4a00fa57b9ee79fd1b08199a7
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-23 16:25:02 +0200

    invalid sso token is rejected

--- src/myservice/MyService.ts
+++ src/myservice/MyService.ts
@@ -1,28 +1,28 @@
-import Response from '../sso/Response'
-import Request from '../sso/Request'
-import SingleSignOnRegistry from '../sso/SingleSignOnRegistry'
-import SSOToken from '../sso/SSOToken'
+import Request from "../sso/Request";
+import Response from "../sso/Response";
+import SingleSignOnRegistry from "../sso/SingleSignOnRegistry";
+import SSOToken from "../sso/SSOToken";
 
 export default class MyService {
   // @ts-ignore
-  private readonly registry?: SingleSignOnRegistry
+  private readonly registry?: SingleSignOnRegistry;
 
   constructor(registry?: SingleSignOnRegistry) {
-    this.registry = registry
+    this.registry = registry;
   }
 
   handleRequest(request: Request): Response {
     // TODO: check request has a valid SSOToken
-    return new Response(`hello ${request.getName()}!`)
+    return new Response("");
   }
 
   handleRegister(username: string, password: string): SSOToken {
     // TODO: register and return token
-    return new SSOToken()
+    return new SSOToken();
   }
 
   handleUnRegister(token: SSOToken) {
     // TODO: unregister token
-    return
+    return;
   }
 }
--- test/MyService.test.ts
+++ test/MyService.test.ts
@@ -1,13 +1,13 @@
-import MyService from '../src/myservice/MyService'
-import Request from '../src/sso/Request'
-import SSOToken from '../src/sso/SSOToken'
+import MyService from "../src/myservice/MyService";
+import Request from "../src/sso/Request";
+import SSOToken from "../src/sso/SSOToken";
 
-describe('MyService', () => {
-  it('invalid sso token is rejected', () => {
-    const service = new MyService(undefined)
+describe("MyService", () => {
+  it("invalid sso token is rejected", () => {
+    const service = new MyService(undefined);
 
-    const response = service.handleRequest(new Request('Foo', new SSOToken()))
+    const response = service.handleRequest(new Request("Foo", new SSOToken()));
 
-    expect(response.getText()).not.toEqual('hello Foo!')
-  })
-})
+    expect(response.getText()).not.toEqual("hello Foo!");
+  });
+});
